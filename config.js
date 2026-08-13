434725bfdbe99f3fce345fe85fa7d6158768863a
Shabid Mods <111037768+Sha787898@users.noreply.github.com>
2026-08-13 21:02:03 +0600

    Add Supabase configuration to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const SUPABASE_URL = "shabidalhasan@gmail.com's Project";
+const SUPABASE_ANON_KEY = "ohvnqbmfjomhtvmzghfq";
+
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
