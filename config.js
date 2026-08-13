ce7343d72643e9f372ef6b30c123a47df2393065
Shabid Mods <111037768+Sha787898@users.noreply.github.com>
2026-08-13 21:03:21 +0600

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const SUPABASE_URL = "shabidalhasan@gmail.com's Project";
+const SUPABASE_URL = "https://ohvnqbmfjomhtvmzghfq.supabase.co";
 const SUPABASE_ANON_KEY = "ohvnqbmfjomhtvmzghfq";
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
