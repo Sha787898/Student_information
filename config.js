4d7d1405aa6d0849d49dbfe8924856d2b2bc5e79
Shabid Mods <111037768+Sha787898@users.noreply.github.com>
2026-08-13 21:34:43 +0600

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const SUPABASE_URL = "https://ohvnqbmfjomhtvmzghfq.supabase.co";
-const SUPABASE_ANON_KEY = "ohvnqbmfjomhtvmzghfq";
+const SUPABASE_ANON_KEY = "sb_publishable_EQorjD1FwRqdiRdoto8f_Q_q4TPTi7t";
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
